17dda97f38f09e5f296d1647c143948b1eff8fa1
SeongHeon Kim <sheon2001@gmail.com>
2026-01-09 03:45:45 +0900

    [fix] apply ktlintFormat

--- src/main/kotlin/com/team10/instagram/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/team10/instagram/global/error/GlobalExceptionHandler.kt
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
-
     // 1. 우리가 직접 정의한 에러 (CustomException) 처리
     @ExceptionHandler(CustomException::class)
-    fun handleCustomException(e: CustomException): ResponseEntity<ApiResponse<Nothing>> {
-        return ResponseEntity
+    fun handleCustomException(e: CustomException): ResponseEntity<ApiResponse<Nothing>> =
+        ResponseEntity
             .status(e.errorCode.status)
             .body(ApiResponse.onFailure(e.errorCode.code, e.errorCode.message))
-    }
 
     // 2. 예상치 못한 모든 시스템 에러 (Exception) 처리
     @ExceptionHandler(Exception::class)
