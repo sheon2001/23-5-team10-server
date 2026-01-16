af7ffba223a4b1ed373b769103bfeedf8a213bbd
SeongHeon Kim <sheon2001@gmail.com>
2026-01-16 16:36:56 +0900

    Update ImageService.kt

--- src/main/kotlin/com/team10/instagram/global/s3/ImageService.kt
+++ src/main/kotlin/com/team10/instagram/global/s3/ImageService.kt
@@ -14,7 +14,6 @@ class ImageService(
     private val bucket: String,
 ) {
     fun upload(files: List<MultipartFile>): List<String> {
-
         // 들어온 파일들을 하나씩 꺼내서 S3에 올리고, URL들을 모아서 반환
         return files.map { file ->
             // 1. 파일 이름 중복 방지 (UUID 사용)
@@ -26,12 +25,13 @@ class ImageService(
 
             // 2. S3에 파일 업로드
             // upload(버킷이름, 저장할이름, 파일의_InputStream, 메타데이터)
-            val resource = s3Template.upload(
-                bucket,
-                savedFileName,
-                file.inputStream,
-                ObjectMetadata.builder().contentType(file.contentType).build()
-            )
+            val resource =
+                s3Template.upload(
+                    bucket,
+                    savedFileName,
+                    file.inputStream,
+                    ObjectMetadata.builder().contentType(file.contentType).build(),
+                )
 
             resource.url.toString()
         }
