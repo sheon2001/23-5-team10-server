d293f936b611d036cb035f878d288cf501033957
SeongHeon Kim <sheon2001@gmail.com>
2026-01-20 22:41:39 +0900

    [fix] set time to KST

--- src/main/kotlin/com/team10/instagram/InstagramCloningApplication.kt
+++ src/main/kotlin/com/team10/instagram/InstagramCloningApplication.kt
@@ -1,10 +1,19 @@
 package com.team10.instagram
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
-class InstagramCloningApplication
+class InstagramCloningApplication {
+    @PostConstruct
+    fun started() {
+        // 시간대 'Asia/Seoul'
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+        println("현재 시간: ${java.time.LocalDateTime.now()}") // 로그 확인용
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<InstagramCloningApplication>(*args)
