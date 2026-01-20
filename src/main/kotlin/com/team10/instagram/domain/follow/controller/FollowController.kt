6dae21a3a740ad42f20b25c4e19123c72fea6740
SeongHeon Kim <sheon2001@gmail.com>
2026-01-20 22:46:50 +0900

    [fix] ktlintFormat

--- src/main/kotlin/com/team10/instagram/domain/follow/controller/FollowController.kt
+++ src/main/kotlin/com/team10/instagram/domain/follow/controller/FollowController.kt
@@ -24,7 +24,7 @@ class FollowController(
     @Operation(summary = "팔로우/언팔로우 요청", description = "팔로우 <-> 언팔로우 전환 요청입니다.")
     fun toggleFollow(
         @PathVariable toUserId: Long,
-        @LoggedInUser loggedInUser: Long
+        @LoggedInUser loggedInUser: Long,
     ): ApiResponse<String> {
         val message = followService.toggleFollow(loggedInUser, toUserId)
 
@@ -36,7 +36,7 @@ class FollowController(
     @Operation(summary = "팔로워 목록 조회", description = "특정 유저를 팔로우 하는 사람들의 목록을 조회합니다.")
     fun getFollowers(
         @PathVariable userId: Long,
-        @LoggedInUser loggedInUser: Long
+        @LoggedInUser loggedInUser: Long,
     ): ApiResponse<List<FollowResponse>> {
         val followers = followService.getFollowers(userId, loggedInUser)
 
@@ -48,7 +48,7 @@ class FollowController(
     @Operation(summary = "팔로잉 목록 조회", description = "특정 유저가 팔로우 하는 사람들의 목록을 조회합니다.")
     fun getFollowings(
         @PathVariable userId: Long,
-        @LoggedInUser loggedInUser: Long
+        @LoggedInUser loggedInUser: Long,
     ): ApiResponse<List<FollowResponse>> {
         val followings = followService.getFollowings(userId, loggedInUser)
 
@@ -60,7 +60,7 @@ class FollowController(
     @Operation(summary = "팔로워 삭제 (강제 언팔)", description = "내 팔로워 목록에서 특정 유저를 삭제합니다. (상대방의 팔로잉 목록에서 내가 사라짐)")
     fun deleteFollower(
         @PathVariable followerId: Long,
-        @LoggedInUser loggedInUser: Long
+        @LoggedInUser loggedInUser: Long,
     ): ApiResponse<Unit> {
         followService.deleteFollower(loggedInUser, followerId)
 
