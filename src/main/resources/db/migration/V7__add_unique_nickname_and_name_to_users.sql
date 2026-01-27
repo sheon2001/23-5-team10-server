94499775c5fa9b5faaf7d33fb8d3ae52a7103fbd
jdahl0711 <jdahl0711@gmail.com>
2026-01-27 19:20:43 +0900

    db V6 -> V7

--- src/main/resources/db/migration/V7__add_unique_nickname_and_name_to_users.sql
+++ src/main/resources/db/migration/V7__add_unique_nickname_and_name_to_users.sql
@@ -1,7 +1,7 @@
 -- nickname UNIQUE 제약 추가
 ALTER TABLE users
-ADD CONSTRAINT uk_users_nickname UNIQUE (nickname);
+    ADD CONSTRAINT uk_users_nickname UNIQUE (nickname);
 
 -- name 컬럼 추가
 ALTER TABLE users
-ADD COLUMN name VARCHAR(50);
+    ADD COLUMN name VARCHAR(50);
