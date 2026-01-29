3a8b9ec698ee1d3fc9df8a2df387d267626618d9
jdahl0711 <jdahl0711@gmail.com>
2026-01-29 19:11:24 +0900

    /users/search q 쿼리 파라메터로 수정

--- src/main/kotlin/com/team10/instagram/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/team10/instagram/domain/user/controller/UserController.kt
@@ -5,7 +5,6 @@ import com.team10.instagram.domain.post.dto.PostResponse
 import com.team10.instagram.domain.post.service.PostService
 import com.team10.instagram.domain.user.LoggedInUser
 import com.team10.instagram.domain.user.dto.UserDto
-import com.team10.instagram.domain.user.dto.UserSearchRequest
 import com.team10.instagram.domain.user.dto.UserSearchResponse
 import com.team10.instagram.domain.user.model.User
 import com.team10.instagram.domain.user.service.UserService
@@ -14,16 +13,18 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import io.swagger.v3.oas.annotations.responses.ApiResponse as SwaggerApiResponse
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/users")
 @Tag(name = "User", description = "사용자 API")
@@ -79,9 +80,9 @@ class UserController(
 
     @GetMapping("/search")
     fun search(
-        @Valid @RequestBody request: UserSearchRequest,
+        @RequestParam @NotBlank q: String,
     ): ApiResponse<UserSearchResponse> {
-        val response = userService.search(request.q)
+        val response = userService.search(q)
         return ApiResponse.onSuccess(response)
     }
 }
